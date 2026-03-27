b848b79fc6017a237b199b22b16de7399ca1c0cc
Muhammad Dabra <muhammaddabra9@gmail.com>
2026-03-27 18:32:12 +0100

    Add  src/Variablesanddatatype.java file

---
+++ src/Variablesanddatatype.java
@@ -0,0 +1,31 @@
+
+
+public class Variablesanddatatype {
+
+        public static void main(String[] args) {
+
+            int num1 = 6;
+            int num2 = 4;
+
+            double percentage = 0.25;
+            double price = 25000.7;
+
+            char currency = '$';
+            char grade = 'A';
+
+            String name = "Muhammad";
+            String height = "he is 1.7m";
+
+            boolean isstudent = false;
+            boolean isAveteran = true;
+
+            if (isstudent) {
+                System.out.println("He could earn "+currency + price);
+            } else {
+                System.out.println(height +  "  "+ "height");
+
+
+            }
+        }}
+
+
