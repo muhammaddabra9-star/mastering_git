40e0a720f83b9eb6ffa8d36299183c1725cfa6d5
Muhammad Dabra <muhammaddabra9@gmail.com>
2026-03-29 11:26:48 +0100

    Add  src/userinput.java file

---
+++ src/userinput.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class userinput {
+    public static void main(String[]args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("what is your name?");
+                 String name=  scanner.nextLine();
+
+                       System.out.println("how old are you");
+                   Integer age=  scanner.nextInt();
+
+                       System.out.println("Are you a student true /false");
+                 boolean isStudent = scanner.nextBoolean();
+
+                       if (isStudent){
+                           System.out.println("ENROLLMENT STATUS: he is eigible");
+
+
+                       }else {
+                           System.out.println("he is not eligible");
+                       }
+
+ System.out.println(" And by the fact that he is  " +age + " years old");
+
+
+
+
+
+    }
+}
